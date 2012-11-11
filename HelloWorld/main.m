29d89d5d5621c673880d067c16ab4a8b0ad6464d
XingyinZhu <xingyinzhu@gmail.com>
2012-11-11 21:48:18 +0800

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Xingyin Zhu on 12-11-11.
+//  Copyright (c) 2012年 Xingyin Zhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
